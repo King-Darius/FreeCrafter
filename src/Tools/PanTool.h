2c4db2b323a7fa43de300357dc783c65581efea0
King-Darius <111044019+King-Darius@users.noreply.github.com>
2025-10-01 00:49:17 +0100

    Refactor navigation drag handling

--- src/Tools/PanTool.h
+++ src/Tools/PanTool.h
@@ -2,7 +2,7 @@
 
 #include "Tool.h"
 
-class PanTool : public Tool {
+class PanTool : public PointerDragTool {
 public:
     PanTool(GeometryKernel* g, CameraController* c);
 
@@ -10,14 +10,5 @@ public:
     bool isNavigationTool() const override { return true; }
 
 protected:
-    void onPointerDown(const PointerInput& input) override;
     void onPointerMove(const PointerInput& input) override;
-    void onPointerUp(const PointerInput& input) override;
-    void onCancel() override;
-
-private:
-    bool dragging = false;
-    int lastX = 0;
-    int lastY = 0;
-    float pixelScale = 1.0f;
 };

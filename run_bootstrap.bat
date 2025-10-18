542017d758d9bf55fe2eb28b721f297780a7d358
King-Darius <111044019+King-Darius@users.noreply.github.com>
2025-10-18 22:23:09 +0100

    corrected typo in bootstrap script

--- run_bootstrap.bat
+++ run_bootstrap.bat
@@ -1,5 +1,9 @@
 @echo off
+rem Configure a Developer Command Prompt and run the Qt bootstrap in place.
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 10.0.26100.0
-set CMAKE_PREFIX_PATH=C:\Qt\6.5.3\msvc2019_64
-set CMAKE_MT=C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\mt.exe
-python scripts\bootstrap.py
+
+set "SCRIPT_ROOT=%~dp0"
+set "CMAKE_PREFIX_PATH=%SCRIPT_ROOT%qt\6.5.3\msvc2019_64"
+set "CMAKE_MT=C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\mt.exe"
+
+python "%SCRIPT_ROOT%scripts\bootstrap.py"
